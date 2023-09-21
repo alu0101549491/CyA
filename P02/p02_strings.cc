7fc74aab44092fc3ff4ac8e4c4d68530b55b6d9b
usuario <alu0101549491@ull.edu.es>
2023-09-21 17:04:23 +0000

    p02 addition

---
+++ P02/p02_strings.cc
@@ -0,0 +1,17 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingeniería y Tecnología
+// Grado en Ingeniería Informática
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Práctica 1: Contenedores asociativos
+// Autor: Fabián González Lence
+// Correo: alu0101549491@ull.edu.es
+// Fecha: 21/09/2023
+// Archivo p02_strings.cc: programa cliente.
+// Contiene la función main del proyecto que usa la clase Cadena
+// para procesar un fichero de texto con identificadores de alumnos y sus notas
+// Referencias:
+// https://github.com/alu0101549491/CyA/P02
+//
+// Historial de revisiones
+// 21/09/2023 - Creación (primera versión) del código
